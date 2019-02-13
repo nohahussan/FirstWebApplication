5abeca0a33e69d4ea652036bb2d60436cef97093
unknown <noha.hussan@gmail.com>
2019-02-13 10:36:01 -0500

    Registration form

---
+++ WebApplication1/Controllers/NewController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: New
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Result(UserInformation u)
+        {
+            ViewBag.FirstName = u.FirstName;
+            ViewBag.Email = u.Email;
+
+            return View();
+        }
+    }   
+}
